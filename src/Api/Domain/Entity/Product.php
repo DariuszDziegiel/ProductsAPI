7910b3121887d2ecb1625655b6a48f712bd6c85e
Dariusz Dzięgiel <dariusz.dziegiel@gmail.com>
2025-04-23 21:27:05 +0200

    feat(api): Add properties and constraints to Product entity

--- src/Api/Domain/Entity/Product.php
+++ src/Api/Domain/Entity/Product.php
@@ -4,12 +4,48 @@ declare(strict_types=1);
 
 namespace Api\Domain\Entity;
 
+use Api\Domain\Entity\Traits\TimestampableTrait;
+use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
+use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\Table;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[Entity]
 #[Table]
+#[HasLifecycleCallbacks]
 class Product
 {
+    use TimestampableTrait;
+
+    #[Id]
+    #[Column(type: 'string')]
+    #[Assert\Uuid()]
+    private string $id;
+
+    #[Column(type: 'string')]
+    #[Assert\NotBlank]
+    private string $title;
+
+    #[Column(type: 'decimal', precision: 10, scale: 2)]
+    #[Assert\PositiveOrZero()]
+    #[Assert\NotBlank]
+    private string $price;
+
+    public function __construct(
+        string $id,
+        string $title,
+        string $price
+    ) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->price = $price;
+    }
+
+    public function id(): string
+    {
+        return $this->id;
+    }
 
 }
