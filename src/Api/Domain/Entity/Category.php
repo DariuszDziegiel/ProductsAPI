57cdc88f0740fb7fabd80b55c3a00a3118088bdd
Dariusz Dzięgiel <dariusz.dziegiel@gmail.com>
2025-04-24 18:13:24 +0200

    feat(doctrine): Add Category entity and many-to-many relationship with Product

--- src/Api/Domain/Entity/Category.php
+++ src/Api/Domain/Entity/Category.php
@@ -4,12 +4,69 @@ declare(strict_types=1);
 
 namespace Api\Domain\Entity;
 
+use Api\Domain\Entity\Traits\TimestampableTrait;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\ManyToMany;
 use Doctrine\ORM\Mapping\Table;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Uuid;
 
 #[Entity]
 #[Table]
+#[HasLifecycleCallbacks]
 class Category
 {
+    use TimestampableTrait;
+
+    #[Id]
+    #[Column(type: 'string', length: 36)]
+    #[Uuid]
+    private string $id;
+
+    #[Column(type: 'string', length: 10, unique: true)]
+    #[Length(min: 1, max: 10)]
+    #[NotBlank]
+    private string $code;
+
+    #[ManyToMany(targetEntity: Product::class, mappedBy: 'categories')]
+    private Collection $products;
+
+    public function __construct(
+        string $id,
+        string $code,
+    ) {
+        $this->id = $id;
+        $this->code = $code;
+        $this->products = new ArrayCollection();
+    }
+
+    public function addProduct(Product $product): self
+    {
+        if (!$this->products->contains($product)) {
+            $this->products->add($product);
+            $product->addCategory($this);
+        }
+        return $this;
+    }
+
+    public function removeProduct(Product $product): self
+    {
+        if ($this->products->removeElement($product)) {
+            $this->removeProduct($product);
+        }
+
+        return $this;
+    }
+
+    public function code(): string
+    {
+        return $this->code;
+    }
 
 }
--- src/Api/Domain/Entity/Product.php
+++ src/Api/Domain/Entity/Product.php
@@ -6,11 +6,15 @@ namespace Api\Domain\Entity;
 
 use Api\Domain\Entity\Traits\TimestampableTrait;
 use Api\Domain\ValueObject\Money;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Embedded;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinTable;
+use Doctrine\ORM\Mapping\ManyToMany;
 use Doctrine\ORM\Mapping\Table;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -22,7 +26,7 @@ class Product
     use TimestampableTrait;
 
     #[Id]
-    #[Column(type: 'string')]
+    #[Column(type: 'string', length: 36)]
     #[Assert\Uuid()]
     private string $id;
 
@@ -33,6 +37,15 @@ class Product
     #[Embedded(class: Money::class)]
     private Money $price;
 
+    #[ManyToMany(
+        targetEntity: Category::class,
+        inversedBy: 'products',
+        cascade: ['persist']
+    )]
+    #[JoinTable(name: 'product_category')]
+    #[Assert\Count(min: 8)]
+    private Collection $categories;
+
     public function __construct(
         string $id,
         string $title,
@@ -41,6 +54,26 @@ class Product
         $this->id = $id;
         $this->title = $title;
         $this->price = $price;
+        $this->categories = new ArrayCollection();
+    }
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories->add($category);
+            //$category->addProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        if ($this->categories->removeElement($category)) {
+            $category->removeProduct($this);
+        }
+
+        return $this;
     }
 
     public function id(): string
