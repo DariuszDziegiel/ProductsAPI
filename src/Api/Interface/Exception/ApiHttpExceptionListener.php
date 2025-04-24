3cd400de1b14b88c710706199b0db86f0261ee14
Dariusz Dzięgiel <dariusz.dziegiel@gmail.com>
2025-04-24 09:24:31 +0200

    refactor(api): Rename and enhance exception listener to handle HTTP 405.
    - Added handling of `MethodNotAllowedHttpException` to return a proper JSON response with allowed methods

--- src/Api/Interface/Exception/ApiHttpExceptionListener.php
+++ src/Api/Interface/Exception/ApiHttpExceptionListener.php
@@ -8,11 +8,12 @@ use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 #[AsEventListener('kernel.exception')]
-class ApiExceptionListener
+class ApiHttpExceptionListener
 {
     public function onKernelException(ExceptionEvent $event): void
     {
@@ -29,6 +30,21 @@ class ApiExceptionListener
             $event->setResponse(new JsonResponse([
                 'message' => $e->getMessage()
             ], Response::HTTP_BAD_REQUEST));
+            return;
         }
+
+        if ($e instanceof MethodNotAllowedHttpException) {
+            $allowedMethods = $e->getHeaders()['Allow'] ?? [];
+            $message = sprintf(
+                "Method Not Allowed. Allowed methods: %s",
+                is_array($allowedMethods) ? implode(',', $allowedMethods) : $allowedMethods
+            );
+
+            $event->setResponse(new JsonResponse([
+                'message' => $message
+            ], Response::HTTP_METHOD_NOT_ALLOWED));
+            return;
+        }
+
     }
 }
