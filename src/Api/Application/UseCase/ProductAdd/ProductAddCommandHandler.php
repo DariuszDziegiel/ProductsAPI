14247847b7b953cd119f05b489acc93a07a904c7
Dariusz Dzięgiel <dariusz.dziegiel@gmail.com>
2025-04-24 14:09:55 +0200

    feat(api): Add event dispatching to ProductAddCommandHandler

--- src/Api/Application/UseCase/ProductAdd/ProductAddCommandHandler.php
+++ src/Api/Application/UseCase/ProductAdd/ProductAddCommandHandler.php
@@ -5,15 +5,20 @@ declare(strict_types=1);
 namespace Api\Application\UseCase\ProductAdd;
 
 use Api\Domain\Entity\Product;
+use Api\Domain\Event\ProductSavedEvent;
 use Api\Domain\Repository\ProductRepositoryInterface;
 use Api\Domain\ValueObject\Money;
+use Symfony\Component\DependencyInjection\Attribute\Target;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 #[AsMessageHandler('command.bus')]
 class ProductAddCommandHandler
 {
     public function __construct(
         private readonly ProductRepositoryInterface $productRepository,
+        #[Target('event.bus')]
+        private readonly MessageBusInterface $eventBus
     ) {}
 
     public function __invoke(ProductAddCommand $command): void
@@ -25,6 +30,10 @@ class ProductAddCommandHandler
         );
 
         $this->productRepository->save($product);
+
+        $this->eventBus->dispatch(
+            ProductSavedEvent::createFromProduct($product)
+        );
     }
 
 }
