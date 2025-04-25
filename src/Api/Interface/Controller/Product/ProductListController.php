8fcc8cf90a2781a0545a1e0e62a8d3bf6870d55a
Dariusz Dzięgiel <dariusz.dziegiel@gmail.com>
2025-04-25 08:33:29 +0200

    feat(api): Add ProductListController

---
+++ src/Api/Interface/Controller/Product/ProductListController.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Interface\Controller\Product;
+
+use Api\Application\UseCase\ProductsList\ProductsListQuery;
+use Api\Application\UseCase\ProductsList\ProductsListQueryHandler;
+use Api\Interface\RequestDTO\ProductsListQueryDTO;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Target;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryString;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+class ProductListController extends AbstractController
+{
+    public function __construct(
+        #[Target('query.bus')]
+        private readonly MessageBusInterface $queryBus,
+    ) {}
+
+    #[Route(
+        '/products',
+        methods: ['GET', 'HEAD']
+    )]
+    public function __invoke(
+        #[MapQueryString] ProductsListQueryDTO $productsListQueryDTO
+    ): JsonResponse
+    {
+        try {
+            $envelope = $this->queryBus->dispatch(
+                new ProductsListQuery(
+                    $productsListQueryDTO->limit,
+                    $productsListQueryDTO->page
+                )
+            );
+            $handled = $envelope->last(HandledStamp::class);
+            $products = $handled->getResult();
+
+            return $this->json($products);
+        } catch (\Throwable $e) {
+            return $this->json([
+                'message' => 'Unexpected error:' . $e->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
