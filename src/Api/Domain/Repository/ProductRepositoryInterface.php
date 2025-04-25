0538cb195c5186dae7508b0472a021cb590f2730
Dariusz Dzięgiel <dariusz.dziegiel@gmail.com>
2025-04-25 13:34:05 +0200

    feat(api): Add findAll method to ProductRepositoryInterface

--- src/Api/Domain/Repository/ProductRepositoryInterface.php
+++ src/Api/Domain/Repository/ProductRepositoryInterface.php
@@ -11,4 +11,6 @@ interface ProductRepositoryInterface
     public function save(Product $product): void;
     public function findById(string $id): ?Product;
     public function remove(Product $product): void;
+    /** @return Product[] */
+    public function findAll(int $limit = 25, int $page = 0): array;
 }
